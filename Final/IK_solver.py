dfb04626cc011b3e2b788ca6e2988e03140936ae
EricYufengWu <yufeng.wu0902@gmail.com>
2020-12-02 12:29:17 -0500

    IK

--- Final/IK_solver.py
+++ Final/IK_solver.py
@@ -24,14 +24,21 @@ class hex_IK:
 	def calculate_angle_yz(self, x0, y0, z0):
 		return
 
+	# Transfer coordinates from base coordinates to individual leg coordinate system
+	def trans_coord(self, x,y,z,leg="FR"):
+		# Does stuff here
+		return x,y,z
 
-	def inverse_k(self, x0, y0, z0):
+	def inverse_k(self, x, y, z):
+
+		x0, y0, z0 = trans_coord(x,y,z)
+
 		theta_c = math.atan(z0/x0)
 
 		r = x0 / math.cos(theta_c)
 		theta_f = math.pi/2 - math.atan((r-self.C)/y0) - math.acos((pow(self.F,2) + y0*y0 + pow((r - self.C),2) - pow(self.T,2)) / (2 * self.F * math.sqrt(y0*y0 + pow((r - self.C),2))))
 
-		theta_t = 0
+		theta_t = 0 # Write stuff here
 
 		print(math.degrees(theta_c), math.degrees(theta_f))
 		return [theta_c, theta_f, theta_t]
